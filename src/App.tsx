37fdda7c21f4e3f7ed67a053fe2b526de24b8c7e
xiaoming <124565124@qq.com>
2022-03-17 16:57:17 +0800

    23

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edit <code>src/App.tsx</code> and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
+        <a className="App-link" href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React1
-        </a>
-      </header>
+        >Learn React1
+        </a></header>
     </div>
   );
 }
